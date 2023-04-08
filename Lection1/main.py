bfd009a044604385db904750759a2949c564c37d
ScarletStranger <maksimledenev6@gmail.com>
2023-04-08 14:49:34 +0300

    first lessons added

---
+++ Lection1/main.py
@@ -0,0 +1,27 @@
+# прописано в консоли python -m venv .folder
+# переменная с пустым значением - n=None
+# как узнать тип переменной (type(переменная))
+"""многострочный
+комментарий"""
+# Ctrl+K Ctrl+C - коммент выделенной области
+# Ctrl+K Ctrl+U - убирает коммент выделенной области
+# Интерполяция print(f"{a}-{b}-{c}")
+# альернативный вывод print("{}-{}-{}".format(a,b,c))
+# a = int(input("Input first number "))
+# b = int(input("Input second number "))
+# print(f"{a} + {b} = {a+b}")
+
+# a=6.119613453
+# b=2.165134545
+# print(round(a*b,3))
+
+# a="qwertyui"
+# for i in a:
+#     print(i)
+
+# text = "i VsyA gRYaz pReVraTilAs V gOLiy lyod"
+# print(len(text)) количество символов строки
+# print ("rY" in text) есть ли в строке введённые символы
+# print(text.lower()) переводит в нижний регистр
+# print(text.upper()) переводит в верхний регистр
+# print (text.replace("VsyA","vsya")) меняет символы слева на символы справа
