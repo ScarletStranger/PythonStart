e20711e0c8bc47bed88b5c25e423e99b3a504072
ScarletStranger <maksimledenev6@gmail.com>
2023-05-02 22:40:37 +0300

    Seminar6 finished

--- Seminar6/main.py
+++ Seminar6/main.py
@@ -17,6 +17,7 @@ for i in range (0, (len(my_list)-1)):
         count +=1
         print (count, my_list[i])
 """
+
 """
 import random
 size = int(input('Введите размер списка '))
@@ -25,7 +26,21 @@ print (my_list)
 print(sum([my_list.count(i)//2 for i in set(my_list)]))
 """
 
-limit = 10000
-def Summa(limit):
-    for i in range(1,limit-1):
-        if 
+# Два различных натуральных числа n и m называются дружественными, если сумма делителей числа n (включая 1, но исключая само n)
+# равна числу m и наоборот.
+# Например числа 220 и 284 - дружественные.
+# Dыведите все пары дружественных чисел до k=10000
+
+n = int(input('Введите число от 1 до 10000 '))
+
+def Summa(n):
+    summ = 0
+    for k in range(1, n//2+1):
+        if n % k == 0:
+            summ += 1
+    return summ
+
+for i in range(1, 10000):
+    j = Summa(i)
+    if i < j <= n and i == Summa(j):
+        print(i,j)
